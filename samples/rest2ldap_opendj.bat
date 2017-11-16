6f8a8ed7f22d836ec08101f622737f7cec408c07
Jason A Everling <jason.everling@gmail.com>
2017-11-16 09:35:46 -0600

    remove testing strings

--- samples/rest2ldap_opendj.bat
+++ samples/rest2ldap_opendj.bat
@@ -1,5 +1,4 @@
-@echo on
+@echo off
 :: Set new password of user in OpenDJ
 :: The uid attribute in OpenDJ has to match sAMAccountName value for person in AD.
 c:\curl\bin\curl.exe --header "Content-Type: application/json" --header "X-OpenIDM-Username: admin-uid" --header "X-OpenIDM-Password: admin-password" -X PATCH "http://opendj.example.com/users/%1" --data "[{\"operation\": \"replace\", \"field\": \"userPassword\", \"value\": \"%2\"}]"
-pause
